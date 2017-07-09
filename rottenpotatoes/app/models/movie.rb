1af67bb534b7682d04db483fbc4ced1f101d8c1c
Bryce <bcrossley@unomaha.edu>
2017-07-09 21:05:14 +0000

    first commit

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,5 +1,17 @@
 class Movie < ActiveRecord::Base
+
+  attr_accessor :title, :rating, :description, :release_date, :director
+ 
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
+  end
+def self.find_with_same_director(id)
+    movie = self.find(id)
+    if not movie.director.empty?
+ 	movies = self.where(:director => movie.director)
+	return [movie, movies, false]
+    else
+	return [movie, [], true]
+    end
   end
 end
